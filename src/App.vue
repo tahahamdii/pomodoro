26422b26d83b806169538344eb170e0f01bf18f1
Hamdi Taha <hamdi.taha@esprit.tn>
2023-04-20 18:52:36 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col sm="6" offset-sm="3">
+            <PomodoroTimer :dialog="dialog" :closeDialog="closeDialog" />
+          </v-col>
+          <v-btn
+            @click="dialog = true"
+            color="secondary"
+            dark
+            small
+            top
+            right
+            fab
+          >
+            <v-icon>mdi-cog-outline</v-icon>
+          </v-btn>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import PomodoroTimer from './components/PomodoroTimer';
+export default {
+  name: 'App',
+  components: {
+    PomodoroTimer,
+  },
+  data() {
+    return {
+      dialog: false
+    }
+  },
+  methods: {
+    closeDialog() {
+      this.dialog = false
+    }
+  }
+};
+</script>
---
+++ src/components/SettingsDialog.vue
@@ -0,0 +1,69 @@
+<template>
+    <v-dialog v-model="showDialog" persistent max-width="600px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">Settings</span>
+        </v-card-title>
+  
+        <v-card-text>
+          <v-text-field
+            v-for="(timer, i) in updatedTimers"
+            :key="i"
+            label="Pomodoro"
+            v-model="updatedTimers[i]"
+            type="number"
+          />
+        </v-card-text>
+  
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="closeDialog">
+            Close
+          </v-btn>
+          <v-btn color="blue darken-1" text @click="save(updatedTimers)">
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      dialog: {
+        type: Boolean,
+        require: true
+      },
+      closeDialog: {
+        type: Function,
+        required: true
+      },
+      timers: {
+        type: Array,
+        required: true
+      },
+      save: {
+        type: Function,
+        required: true
+      }
+    },
+    data() {
+      return {
+        updatedTimers: [],
+        showDialog: false
+      }
+    },
+    mounted() {
+      this.updatedTimers = this.timers.map((timer) => {
+        return timer.minutes
+      })
+      this.showDialog = this.dialog
+    },
+    watch: {
+      dialog(newVal) {
+        this.showDialog = newVal
+      }
+    }
+  }
+  </script>
